c8dfec335f6e6d8d3fe8625a9f4512227f966c66
davitmartirosyan <phpencoding@gmail.com>
2023-09-21 02:35:54 +0400

    42-yerevan-webserv

---
+++ Makefile
@@ -0,0 +1,24 @@
+NAME = webserv
+CC = c++
+CC_FLAGS = -std=c++98 -Wall -Wextra -Werror
+SRCS = $(shell find src/ -type f -name "*.cpp") $(NAME).cpp
+OBJS = $(SRCS:.cpp=.o)
+INCLUDES = $(shell find includes/ -type f -name "*.hpp")
+
+all: $(NAME)
+
+$(NAME) : $(OBJS) $(INCLUDES)
+	$(CC) $(CC_FLAGS) $(OBJS) -o $(NAME)
+
+%.o : %.cpp $(INCLUDES)
+	$(CC) $(CC_FLAGS) -c $< -o $@
+
+fclean: clean
+	@rm -rf $(NAME)
+
+clean:
+	@rm -rf $(OBJS)
+
+re:fclean all
+
+.PHONNY: all fclean re clean 
---
+++ webserv.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmartiro <dmartiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 02:05:48 by dmartiro          #+#    #+#             */
+/*   Updated: 2023/09/21 02:33:25 by dmartiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <fcntl.h>
+
+int main(int ac, char ** av, char **env)
+{
+    (void)ac;
+    (void)av;
+    (void)env;
+}
