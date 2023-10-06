1132fa0c4236a4ad33286b77b0ab6bd9e95a5dfe
davitmartirosyan <phpencoding@gmailcom>
2023-10-06 13:10:17 +0400

    42-yerervan-webserv Day16

--- includes/HTTPServer.hpp
+++ includes/HTTPServer.hpp
@@ -6,7 +6,7 @@
 /*   By: dmartiro <dmartiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 16:44:02 by dmartiro          #+#    #+#             */
-/*   Updated: 2023/10/06 01:03:28 by dmartiro         ###   ########.fr       */
+/*   Updated: 2023/10/06 13:08:22 by dmartiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,14 @@ class HTTPServer
     public:
         HTTPServer(std::string const &ipv4, int domain, int type, int protocol, int port, int backlog);
         ~HTTPServer();
-    public: //interface
+    public: //external interface
         HTTPRequest request;
+    public: //interface
+        int getServerSocket( void ) const;
     private:
         std::map<unsigned int, HTTPRequest> clients;
     private:
         struct  sockaddr_in remoteAddr;
-        struct sockaddr a;
     private:
         std::string ipv4;
     private:
@@ -40,3 +41,4 @@ class HTTPServer
 };
 
 #endif
+
--- src/HTTPServer.cpp
+++ src/HTTPServer.cpp
@@ -6,7 +6,7 @@
 /*   By: dmartiro <dmartiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 16:45:11 by dmartiro          #+#    #+#             */
-/*   Updated: 2023/10/06 01:21:28 by dmartiro         ###   ########.fr       */
+/*   Updated: 2023/10/06 12:54:39 by dmartiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ HTTPServer::HTTPServer(std::string const &ipv4, int domain, int type, int protoc
     this->port = port;
     this->server_socket = socket(domain, type, protocol);
     this->backlog = backlog;
-    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
     if (server_socket < 0)
         throw ServerException("Socket");
+    // if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR & SO_REUSEPORT, &opt, sizeof(opt)) < 0)
+    //     throw ServerException("Setsockopt");
     remoteAddr.sin_family = domain;
-    remoteAddr.sin_addr.s_addr = inet_addr(ipv4.c_str());
+    remoteAddr.sin_addr.s_addr = INADDR_ANY;
     remoteAddr.sin_port = htons(port);
     if (bind(server_socket, (struct sockaddr*)&remoteAddr, sizeof(remoteAddr)) < 0)
         throw ServerException("Bind");
@@ -35,4 +36,9 @@ HTTPServer::HTTPServer(std::string const &ipv4, int domain, int type, int protoc
 
 HTTPServer::~HTTPServer()
 {
+}
+
+int HTTPServer::getServerSocket( void ) const
+{
+    return (this->server_socket);
 }
